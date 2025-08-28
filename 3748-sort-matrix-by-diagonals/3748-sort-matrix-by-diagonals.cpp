03607d68abf07284a46ea0105c0acfa0bda3992f
Karan Gautam <karangautam870@gmail.com>
2025-08-28 08:30:34 +0530

    Time: 2 ms (94.03%), Space: 45.1 MB (36.01%) - LeetHub

--- 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
@@ -6,12 +6,14 @@ public:
         // bottom left
         for (int i = 0; i < n; i++) {
             vector<int> dia;
+
             int x = i, y = 0;
             while (x < n && y < n) {
                 dia.push_back(grid[x][y]);
                 x++;
                 y++;
             }
+
             sort(dia.rbegin(), dia.rend());
             
             x = i, y = 0;
@@ -24,6 +26,7 @@ public:
         // top left
         for (int j = 1; j < n; j++) {
             vector<int> dia;
+            
             int x = 0, y = j;
             while (x < n && y < n) {
                 dia.push_back(grid[x][y]);
