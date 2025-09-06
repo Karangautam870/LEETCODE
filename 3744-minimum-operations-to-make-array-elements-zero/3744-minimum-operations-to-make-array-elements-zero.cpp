557997ebba6812996c9f1d684d7cb2710cad5096
Karan Gautam <karangautam870@gmail.com>
2025-09-06 23:48:49 +0530

    Time: 9 ms (84.06%), Space: 183.2 MB (78.99%) - LeetHub

--- 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.cpp
@@ -1,23 +1,24 @@
-#define ll long long 
+#define ll long long
+
 class Solution {
 public:
-    ll solve(int l, int r) {
+    ll solve(ll l, ll r) {
         ll L = 1;
         ll S = 1;
         ll steps = 0;
 
-        while(L <= r) {
-            ll R = 4*L - 1;
+        while (L <= r) {
+            ll R = 4 * L - 1;
 
-            ll start = max(L, (ll)l);
-            ll end   = min(R, (ll)r);
+            ll start = max(L, l);
+            ll end = min(R, r);
 
-            if(start <= end) {
-                steps += (end-start+1)*S;
+            if (start <= end) {
+                steps += (end - start + 1) * S;
             }
 
             S += 1;
-            L = L*4;
+            L *= 4;
         }
 
         return steps;
@@ -26,13 +27,13 @@ public:
     long long minOperations(vector<vector<int>>& queries) {
         ll ans = 0;
 
-        for(auto &query : queries) {
+        for (auto& query : queries) {
             int l = query[0];
             int r = query[1];
 
             ll steps = solve(l, r);
 
-            ans += (steps+1)/2;
+            ans += ceil(steps / 2.0);
         }
 
         return ans;
