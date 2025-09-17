810df084bda49fe8684673aafc8b5fa65630e309
Karan Gautam <karangautam870@gmail.com>
2025-09-17 18:40:29 +0530

    Time: 123 ms (80.61%), Space: 170.5 MB (63.99%) - LeetHub

---
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
@@ -0,0 +1,51 @@
+class FoodRatings {
+public:
+    unordered_map<string, int> mp;
+    unordered_map<string, string> foodToCountry;
+
+    struct Compare {
+        bool operator()(const pair<int, string>& a,
+                        const pair<int, string>& b) const {
+            if (a.first == b.first)
+                return a.second > b.second;    
+            return a.first < b.first; 
+        }
+    };
+
+    map<string,
+        priority_queue<pair<int, string>, vector<pair<int, string>>, Compare>>
+        mps;
+
+
+    void changeRating(string food, int newRating) {
+        mp[food] = newRating;
+        string nation = foodToCountry[food];
+        mps[nation].push({newRating, food});
+    }
+
+    string highestRated(string country) {
+        auto& pq = mps[country];
+
+        while (!pq.empty()) {
+            auto top = pq.top();
+
+            if (mp[top.second] == top.first) {
+                return top.second;
+            }
+            pq.pop();
+        }
+        return "";
+    }
+
+    FoodRatings(vector<string>& foods, vector<string>& country,
+                vector<int>& ratings) {
+        int n = foods.size();
+
+        for (int i = 0; i < n; i++) {
+            mp[foods[i]] = ratings[i];
+            foodToCountry[foods[i]] = country[i];
+            mps[country[i]].push({ratings[i], foods[i]});
+        }
+
+    }
+};
