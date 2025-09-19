38c0d1b879075503abef48d8e8d314176ffd026b
Karan Gautam <karangautam870@gmail.com>
2025-09-19 21:59:42 +0530

    Time: 67 ms (81.38%), Space: 120.6 MB (96.28%) - LeetHub

--- 3797-design-spreadsheet/3797-design-spreadsheet.cpp
+++ 3797-design-spreadsheet/3797-design-spreadsheet.cpp
@@ -1,92 +1,26 @@
 class Spreadsheet {
 public:
-    
-    vector<vector<int>> arr;
-    
-    void setCell(string cell, int value) {
-        int col = tolower(cell[0]) - 'a';
-        int row = 0;
-        for(int i = 1;i < cell.size();i++){
-            row = row * 10 + cell[i] - '0';
-        }
+    unordered_map<string, int> mp;
 
-        arr[row][col] = value;
-        // cout<<row<< ","<<col<<" - " << value << endl;
-    }
-    
-    void resetCell(string cell) {
-        int col = tolower(cell[0]) - 'a';
-        int row = 0;
-
-        for(int i = 1;i < cell.size();i++){
-            row = row * 10 + cell[i] - '0';
-        }
-
-        arr[row][col] = 0;
-    }
-    
-    int getValue(string s) {
-        string s1 = "" , s2 = "";
-        int i = 1;
-
-        // s1
-        while(i < s.size() && s[i] != '+'){
-            s1 += s[i];
-            i++;
-        }
-        // cout<<s1<<endl;
-        i++;
-        // s2
-        while(i < s.size()){
-            s2 += s[i];
-            i++;
-        }
-        // cout<<s2<<endl;
-
-        i = 1;
-
-        int ans = 0;
-        int col1 = 0 , row1 = 0,sum1 = 0;
-        int col2 = 0 , row2 = 0,sum2 = 0;
-
-        // s1
-        if(isalpha(s1[0])){
-            col1  = tolower(s1[0]) - 'a';
-            while(i < s1.size()){
-                row1 = row1 * 10 + s1[i] - '0';
-                i++;
-            }
-            ans += arr[row1][col1];
-        }else{
-            i = 0;
-            while(i < s1.size()){
-                sum1 = sum1 * 10 + s1[i] - '0';
-                i++;
-            }
-            ans += sum1;
-        }
-
-        i = 1;
-        // s2
-        if(isalpha(s2[0])){
-            col2  = tolower(s2[0]) - 'a';
-            while(i < s2.size()){
-                row2 = row2 * 10 + s2[i] - '0';
-                i++;
-            }
-            ans += arr[row2][col2];
-        }else{
-            i = 0;
-            while(i < s2.size()){
-                sum2 = sum2 * 10 + s2[i] - '0';
-                i++;
-            }
-            ans += sum2;
-        }
-        return ans;
+    void setCell(string cell, int value) { 
+        mp[cell] = value; 
     }
 
-    Spreadsheet(int rows) {
-        arr.assign(rows + 2,vector<int>(26,0));
+    void resetCell(string cell) { 
+        mp.erase(cell); 
     }
+
+    int getValue(string formula) {
+        int pos = formula.find('+');
+        string cell1 = formula.substr(1, pos - 1);
+        string cell2 = formula.substr(pos + 1);
+
+        int val1 = isalpha(cell1[0]) ? (mp.count(cell1) ? mp[cell1] : 0)
+                                     : stoi(cell1);
+        int val2 = isalpha(cell2[0]) ? (mp.count(cell2) ? mp[cell2] : 0)
+                                     : stoi(cell2);
+
+        return val1 + val2;
+    }
+    Spreadsheet(int rows) {}
 };
