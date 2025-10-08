1a3e51d40e77ad0aa2525a368dc613bfc9f9a790
Karan Gautam <karangautam870@gmail.com>
2025-10-08 09:58:58 +0530

    Time: 36 ms (91.42%), Space: 125.1 MB (81.16%) - LeetHub

---
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(vector<int>& p, int num, long long k) {
+        int s = 0, e = p.size() - 1;
+        int ans = p.size();
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (1LL * p[mid] * num >= k) {
+                ans = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return p.size() - ans;
+    }
+    vector<int> successfulPairs(vector<int>& s, vector<int>& p, long long k) {
+
+        vector<int> ans(s.size(), 0);
+        sort(p.begin(), p.end());
+
+        unordered_map<int, int> mp;
+        for (int i = 0; i < s.size(); i++) {
+            ans[i] = solve(p, s[i], k);
+        }
+
+        return ans;
+    }
+};
