44918a290510672657da61813ebea09bec2f4b27
Karan Gautam <karangautam870@gmail.com>
2025-10-15 08:04:51 +0530

    Time: 230 ms (50.38%), Space: 182.1 MB (36.09%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,60 +1,6 @@
-// class Solution {
-// public:
-//     bool isValid(vector<int>& nums, int k) {
-//         int n = nums.size();
-
-//         for (int i = 0; i <= n - 2 * k; i++) {
-//             bool firstInc = true, secondInc = true;
-
-//             for (int j = i; j < i + k - 1; j++) {
-//                 if (nums[j] >= nums[j + 1]) {
-//                     firstInc = false;
-//                     break;
-//                 }
-//             }
-
-//             if (firstInc) {
-//                 for (int j = i + k; j < i + 2 * k - 1; j++) {
-//                     if (nums[j] >= nums[j + 1]) {
-//                         secondInc = false;
-//                         break;
-//                     }
-//                 }
-
-//                 if (secondInc)
-//                     return true;
-//             }
-//         }
-
-//         return false;
-//     }
-
-//     int maxIncreasingSubarrays(vector<int>& nums) {
-        
-//         int n = nums.size();
-//         int l = 1, r = n / 2;
-//         int maxK = 0;
-
-//         while (l <= r) {
-//             int mid = l + (r - l) / 2;
-
-//             if (isValid(nums, mid)) {
-//                 maxK = mid;
-//                 l = mid + 1;
-//             } else {
-//                 r = mid - 1;
-//             }
-//         }
-
-//         return maxK;
-//     }
-// };
-
-
-
 class Solution {
 public:
-    void precomputeIncreasingSubarrays(vector<int>& nums, vector<int>& inc) {
+    void IncSub(vector<int>& nums, vector<int>& inc) {
         int n = nums.size();
         inc[0] = 1;  
         for (int i = 1; i < n; i++) {
@@ -66,10 +12,9 @@ public:
         }
     }
 
-    bool isValid(const vector<int>& nums, int k, const vector<int>& inc) {
+    bool solve(const vector<int>& nums, int k, const vector<int>& inc) {
         int n = nums.size();
         
-
         for (int i = 0; i <= n - 2 * k; i++) {
 
             if (inc[i + k - 1] >= k && inc[i + 2 * k - 1] >= k) {
@@ -87,13 +32,13 @@ public:
 
 
         vector<int> inc(n, 0);
-        precomputeIncreasingSubarrays(nums, inc);  
+        IncSub(nums, inc);  
 
 
         while (l <= r) {
             int mid = l + (r - l) / 2;  
 
-            if (isValid(nums, mid, inc)) {
+            if (solve(nums, mid, inc)) {
                 maxK = mid;  
                 l = mid + 1;
             } else {
