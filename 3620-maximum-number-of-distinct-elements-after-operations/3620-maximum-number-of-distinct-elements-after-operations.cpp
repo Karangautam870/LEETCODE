299b016624489ac4f4cb09d19631470a1204a70c
Karan Gautam <karangautam870@gmail.com>
2025-10-18 08:29:47 +0530

    Time: 114 ms (79.49%), Space: 97.4 MB (98.74%) - LeetHub

---
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+
+        int ans = 0, prev = INT_MIN;
+        for (int num : nums) {
+            int curr = min(max(num - k, prev + 1), num + k);
+            if (curr > prev) {
+                ans++;
+                prev = curr;
+            }
+        }
+        return ans;
+        
+    }
+};
