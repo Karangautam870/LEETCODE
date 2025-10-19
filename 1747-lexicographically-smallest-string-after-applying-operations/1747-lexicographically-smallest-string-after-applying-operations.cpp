dd1723c9258c96fa74e620a1050b1e8b6b295057
Karan Gautam <karangautam870@gmail.com>
2025-10-19 22:40:13 +0530

    Time: 318 ms (27.87%), Space: 95.6 MB (42.03%) - LeetHub

--- 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.cpp
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.cpp
@@ -1,35 +1,53 @@
 class Solution {
 public:
     string ans;
-    unordered_set<string>st;
+    unordered_set<string> st;
     int n;
 
-    void bfs(string s, int a, int b){
-        
-        queue<string>q;
+    void dfs(string curr, int a, int b) {
+        st.insert(curr);
+        ans = min(curr, ans);
+
+        string temp = curr;
+        for (int i = 1; i <= n; i += 2) {
+            temp[i] = (temp[i] - '0' + a) % 10 + '0';
+        }
+        if (st.count(temp) == 0) {
+            dfs(temp, a, b);
+        }
+        string k = curr.substr(b) + curr.substr(0, b);
+
+        if (st.count(k) == 0) {
+            dfs(k, a, b);
+        }
+    }
+
+    void bfs(string s, int a, int b) {
+        queue<string> q;
         q.push(s);
 
-        while(!q.empty()){
+        while (!q.empty()) {
             string curr = q.front();
             q.pop();
 
-            ans = min(curr,ans);
-            
+            ans = min(curr, ans);
+
             // add
             string temp = curr;
-            for(int i = 1; i <= n;i+=2){
+            for (int i = 1; i <= n; i += 2) {
                 temp[i] = (temp[i] - '0' + a) % 10 + '0';
             }
 
-            if(st.count(temp) == 0){
+            if (st.count(temp) == 0) {
                 q.push(temp);
                 st.insert(temp);
             }
 
             // rotate
-            string k = curr.substr(b) + curr.substr(0,b);
-            
-            if(st.count(k) == 0){
+            // substr(pos, len)
+            string k = curr.substr(b) + curr.substr(0, b);
+
+            if (st.count(k) == 0) {
                 q.push(k);
                 st.insert(k);
             }
@@ -39,7 +57,8 @@ public:
     string findLexSmallestString(string s, int a, int b) {
         n = s.size();
         ans = s;
-        bfs(s,a,b);
+        // bfs(s,a,b);
+        dfs(s, a, b);
         return ans;
     }
 };
